b30902881af167fd855adb40f3eb4137362ad4d8
lebrancconvas <lebrancconvas@gmail.com>
2025-01-01 21:12:39 +0700

    ✨ feat: update solution for aoc_11 in typescript (WIP)

--- solution/typescript/src/solution/aoc_03/index.ts
+++ solution/typescript/src/solution/aoc_03/index.ts
@@ -1,4 +1,4 @@
-import { match } from "assert";
+import { matchesGlob } from "path";
 import { getInput } from "../../helper/input";
 
 export function aoc_03_1(): number {
@@ -16,4 +16,37 @@ export function aoc_03_1(): number {
   return sum;
 };
 
+export function aoc_03_2() {
+  const input = getInput('aoc_03');
+
+  const parts = input.split(/(don't\(\)|do\(\))/);
+
+  let shouldProcess = true;
+
+  const pattern = /mul\((\d+),(\d+)\)/g;
+
+
+  let sum = 0;
+  parts.forEach((part, index) => {
+    if(part === "don't()") {
+      shouldProcess = false;
+    } else if(part === "do()") {
+      shouldProcess = true;
+    } else {
+      if(shouldProcess) {
+        const operations = [...part.matchAll(pattern)];
+        operations.forEach(operation => {
+          const [_, num1, num2] = operation;
+
+          sum += parseInt(num1) * parseInt(num2);
+        })
+      }
+    }
+  });
+
+  return sum;
+};
+
+
 console.log(aoc_03_1());
+console.log(aoc_03_2());
